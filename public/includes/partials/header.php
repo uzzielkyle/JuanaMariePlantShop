b3d42c053c4ab77505a1d5ae445b6c1341c5552b
UzziKyle <uzzielkyleynciong0824@gmail.com>
2025-05-29 07:25:02 +0800

    feat(nav): add href to nav

--- public/includes/partials/header.php
+++ public/includes/partials/header.php
@@ -6,7 +6,7 @@ $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https:
 $host = $_SERVER['HTTP_HOST'];
 
 // Project base folder name
-$base = "/JuanaMariePlantShop"; // Change if your root folder name differs
+$base = "/JuanaMariePlantShop/public"; // Change if your root folder name differs
 
 // Final base URL
 $base_url = $protocol . $host . $base;
@@ -15,7 +15,7 @@ $base_url = $protocol . $host . $base;
 
 <header class="navbar navbar-expand-lg bg-white px-4 py-2 border-bottom fixed-top">
   <a class="navbar-brand d-flex align-items-center me-5" href="#">
-    <img src="<?= $base_url ?>/public/assets/logo.svg" alt="Logo" style="height: 40px;">
+    <img src="<?= $base_url ?>/assets/logo.svg" alt="Logo" style="height: 40px;">
   </a>
 
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
@@ -25,24 +25,24 @@ $base_url = $protocol . $host . $base;
   <div class="collapse navbar-collapse" id="mainNavbar">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active fw-bold px-4 mx-2 rounded-pill bg-success-subtle" href="#">HOME</a>
+        <a class="nav-link active fw-bold px-4 mx-2 rounded-pill bg-success-subtle" href="<?= $base_url ?>/">HOME</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link px-4 mx-2" href="#">SHOP</a>
+        <a class="nav-link px-4 mx-2" href="<?= $base_url ?>/shop.php">SHOP</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link px-4 mx-2" href="#">ABOUT US</a>
+        <a class="nav-link px-4 mx-2" href="<?= $base_url ?>/about.php">ABOUT US</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link px-4 mx-2" href="#">CONTACT US</a>
+        <a class="nav-link px-4 mx-2" href="<?= $base_url ?>/contacts.php">CONTACT US</a>
       </li>
     </ul>
 
     <div class="d-flex align-items-center gap-3">
-      <a href="#" class="text-dark fs-5">
+      <a href="<?= $base_url ?>/user/cart.php" class="text-dark fs-5">
         <i class="bi bi-cart"></i>
       </a>
-      <a href="#">
+      <a href="<?= $base_url ?>/user-account.php">
         <img src="http://placebeard.it/250/250" alt="Profile" class="rounded-circle" style="height: 32px; width: 32px;">
       </a>
     </div>
